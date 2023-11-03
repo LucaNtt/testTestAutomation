171af956915a12607c5e1dd757dce0ef37e86908
luca.seccia@nttdata.com <luca.seccia@nttdata.com>
2023-11-03 18:15:28 +0100

    delete test checklink

--- cypress/e2e/checkLinks.cy.js
+++ cypress/e2e/checkLinks.cy.js
@@ -1,17 +1,31 @@
 describe('Test links on video.sky.it', () => {
   it('Controlla che tutti i links siano validi', () => {
+    var linkNoWork = []
     cy.visit('/')
-    cy.get('a')
-    .each((link) => {
-      //if (link.prop('href') == ''|| link.prop('href').endsWith(']') || link.prop('href') == 'https://www.sky.it/come-vedere/app.html' || link.prop('href') == 'https://www.sky.it/assistenza/moduli-contrattuali.html' || link.prop('href') == 'https://www.sky.it/assistenza/trasparenza-tariffaria.html' || link.prop('href') == 'https://www.sky.it/info/modulo_segnalazione_abusi.html') {
-      if (link.prop('href') == '') {
-        return;
-      }
-      cy.request(link.prop('href'))
-      .its('status')
-      .should('not.eq', 404)
 
-    })
+    cy.get('a')
+      .each((link) => {
+        cy.request({
+          url: link.prop('href'),
+          failOnStatusCode: false
+        })
+          .its('status')
+          .then((item) => {
+            console.log('boooot ' + item)
+            if (item === 404) {
+              linkNoWork.push(link.prop('href'))
+            }
+          })
+
+        console.log('1link no work have at least an item? ' + linkNoWork[0])
+      }).then(() => {
+        for (let i = 0; i < linkNoWork.length; i++) {
+          console.log('be? ' + linkNoWork[i]);
+        }
+          let test = linkNoWork.join(', ');
+          expect(linkNoWork.length,`The following links doesn't work:  ${linkNoWork.join(', ')}`).to.equal(0);
+        
+      })
   })
 })
 
